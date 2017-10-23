058cfbba4648c70e617093c07f0a29aa55233e7e
micaela berry <msberry9@gmail.com>
2017-10-23 14:32:58 -0400

    pratice

---
+++ script.js
@@ -0,0 +1,6 @@
+var userFirstName = prompt("What is your First Name?");
+var userLastName = prompt("What is your Last Name?");
+var userAge = prompt("What is your Age?");
+
+console.log("Your full name is " + userFirstName + " " + userLastName + ".");
+console.log("You are " + userAge); 
