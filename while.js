8888eb55d861cfba0829d98a67652f52211afabd
micaela berry <msberry9@gmail.com>
2017-11-01 14:26:32 -0400

    More practice

--- while.js
+++ while.js
@@ -7,21 +7,35 @@ counter++
 
 console.log("Printing even numbers between 10 and 40");
 var counter = 10;
-while (counter < 40){
+
+while (counter =< 40){
 console.log(counter);
 counter+=2;
 }
 
-while(counter <= 40){
+/*while(counter <= 40){
 	if(counter % 2 === 0){
+		console.log(counter);
+	}
+	counter+=1;
+}*/
+
+console.log("Printing odd numbers between 300 and 333");
+var counter = 300;
+
+while(counter <= 333){
+	if(counter % 2 !== 0){
 		console.log(counter);
 	}
 	counter+=1;
 }
 
-/*console.log("Printing odd numbers between 300 and 333");
+onsole.log("Printing odd numbers between 300 and 333");
 var counter = 300;
-while (counter < 333){
-console.log(counter);
-counter+=1;
-}*/
+
+while(counter <= 333){
+	if(counter % 2 !== 0){
+		console.log(counter);
+	}
+	counter+=1;
+}
